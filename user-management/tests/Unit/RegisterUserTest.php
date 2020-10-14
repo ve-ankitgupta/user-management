6532b9011127488cc4d6ae159692eab14b7db7f0
ankit gupta <ankitgupta1@virtualemployee.com>
2020-10-14 14:59:16 +0530

    comment all test cases for while

--- user-management/tests/Unit/RegisterUserTest.php
+++ user-management/tests/Unit/RegisterUserTest.php
@@ -4,15 +4,45 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+// use Faker\Generator as Faker;
+
 class RegisterUserTest extends TestCase
 {
     /**
-     * A basic unit test example.
-     *
-     * @return void
+     * @var Illuminate\Foundation\Testing\WithFaker
+     * @var Illuminate\Foundation\Testing\RefreshDatabase;
      */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+    use WithFaker, RefreshDatabase;
+
+    protected $admin;
+
+    // public function setUp():void {
+    //     $this->faker = new Faker;
+    //     parent::setUp();
+    //     $this->addAdmin();
+    // }
+
+    // private function addAdmin() {
+    //     $this->admin = User::create([
+    //         'name' => $this->faker->name,
+    //         'email' => $faker->unique()->safeEmail,
+    //         'password' => $this->faker->password,
+    //         'role' => 'admin',
+    //         'status' => true
+    //     ]);
+    // }
+
+    // public function testRegisterUserSuccess() {
+    //     dd(get_class_methods($this->faker));
+    //     $this->actingAs($this->admin)->post('/users/register', [
+    //         'name' => $this->faker->name,
+    //         'email' => $faker->unique()->safeEmail,
+    //         'password' => $this->faker->password,
+    //         'role' => $this->faker->userName,
+    //         'status' => $this->faker->boolean
+    //     ])->assertRedirect('/users');
+    // }
 }
